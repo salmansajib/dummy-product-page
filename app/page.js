94b9eed70730f3c3c31b6b520ef3f9c6f433335d
Salman-sajib <sfsajib@gmail.com>
2025-01-31 22:25:18 +0600

    added some data

--- app/page.js
+++ app/page.js
@@ -19,32 +19,34 @@ export default function Home() {
   if (isError) return <p>Error loading products: {error.message}</p>;
 
   return (
-    <div className="p-4 container mx-auto">
-      <h1 className="text-2xl font-bold mb-5">Products</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4 ">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="bg-gray-100 p-4 rounded-md space-y-2 group"
-          >
-            <div className="overflow-hidden rounded-md">
-              <Image
-                src={product.image}
-                alt={product.name}
-                width={300}
-                height={300}
-                quality={80}
-                placeholder="blur"
-                blurDataURL={product.blurDataUrl}
-                className="w-full h-40 object-cover group-hover:scale-105 transition-all duration-300"
-              />
+    <div className="p-6 bg-slate-50 min-h-screen">
+      <main className="container mx-auto">
+        <h1 className="text-2xl font-bold mb-5">Products</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-teal-100 p-4 rounded-md space-y-2 group"
+            >
+              <div className="overflow-hidden rounded-md">
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={300}
+                  height={300}
+                  quality={80}
+                  placeholder="blur"
+                  blurDataURL={product.blurDataUrl}
+                  className="w-full h-40 object-cover group-hover:scale-105 transition-all duration-300"
+                />
+              </div>
+              <h2 className="text-lg font-semibold">{product.name}</h2>
+              <p className="text-[14px]">{product.description}</p>
+              <p className="text-gray-600">${product.price}</p>
             </div>
-            <h2 className="text-lg font-semibold">{product.name}</h2>
-            <p className="text-[14px]">{product.description}</p>
-            <p className="text-gray-600">${product.price}</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
